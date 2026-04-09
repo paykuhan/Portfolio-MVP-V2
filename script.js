f92124e7a608f3444d63b809b138fe0f42431632
Dawillams <dawillams15@gmail.com>
2026-04-08 21:26:51 -0300

    Update project details in script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Array de projetos
 const projetos = [
   {
-    titulo: "Portfólio Profissional v2",
+    titulo: "Portfólio Profissional V1",
     descricao: "Site pessoal moderno focado em UI/UX, utilizando HTML5, CSS3 avançado e JavaScript.",
     tecnologias: ["html5", "css3-alt", "js"], // ícones do FontAwesome (sem o fa-)
-    link: "https://github.com/paykuhan/Portfolio-MVP"
+    link: "https://paykuhan.github.io/Portfolio-MVP-V1/"
   },
   {
     titulo: "Calculadora Minimalista",
